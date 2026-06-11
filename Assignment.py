0b5959a3a5d3202679a38562c431367b54649e51
manuel090100 <manuel009oe@gmail.com>
2026-06-11 10:34:59 +0100

    assignment

---
+++ Assignment.py
@@ -0,0 +1,8 @@
+def equation(a,b,c):
+    x=(-b+(b**2)-4*a*c)/2*a
+    y=(-b-(b**2)-4*a*c)/2*a
+    print(f"The solution sets of the equation are \n {x} \n and \n {y}")
+
+
+equation(1,-2,1)    
+
---
+++ up.py
@@ -0,0 +1,4 @@
+drivers=[1,4,8,2]
+print(drivers[0])
+print(drivers[2])
+print(drivers[3])
