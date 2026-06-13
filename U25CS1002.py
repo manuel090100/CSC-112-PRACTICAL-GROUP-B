4fbd057bafedc648e5db21e52e1f2084434a298b
Michael Aku_Attah <267376475+NerdBlaq@users.noreply.github.com>
2026-06-13 22:32:01 +0100

    Initial commit

---
+++ U25CS1002.py
@@ -0,0 +1,7 @@
+def almighty (a,b,c):
+    x=(-b+(b**2)-4*a*c)/2*a
+    y=(+b+(b**2)-4*a*c)/2*a
+    print(f"The Valuse of x and y are \nX={x}\nY={y}")
+almighty(1,-2,1)
+print("Where X is the Negative value of the Almighty Formula \nAnd Y is the Positive value of the Almighty Formula ")
+print("By U25CS1002 \nAku-Attah, Michael Onu \nComputer Science Department \n100 Level")
